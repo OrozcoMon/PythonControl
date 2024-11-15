7b04ece518183d039dcded08f582b22af10475ba
OrozcoMon <montse4oral@gmail.com>
2024-11-15 12:55:33 +0100

    pikachu

---
+++ 3.py
@@ -0,0 +1,7 @@
+#Eliminar totes les aparicions de LRU en una seqüència de moviments
+
+
+cadena=input("Insereix la seqüència: ")
+cadena=cadena.upper()
+cadena=cadena.replace("LRU"," ")
+print(cadena)
