68030df670d36a6f812cf5f2d8e76848f918dd5f
OrozcoMon <montse4oral@gmail.com>
2024-11-15 12:52:29 +0100

    pikachu

---
+++ 2.1.py
@@ -0,0 +1,13 @@
+#Els moviments que realitza un braç robot estan codificats amb L,R,U i D. Ens entren
+#una seqüència de moviments i volem saber si més de la meitat són U.
+
+#Version1
+
+cadena=input("Insereix la seqüència: ")
+cadena=cadena.upper()
+cadenaU=cadena.count("U")
+cadena=len(cadena)
+if cadenaU > cadena / 2:
+    print("Hi ha més de la meitat", cadenaU)
+else:
+    print("No hi ha més de la mitad", cadenaU)
