1f495c22c05c287954af8565df7a6e233c9df1b1
OrozcoMon <montse4oral@gmail.com>
2024-11-26 18:37:39 +0100

    charizard

--- 16.py
+++ 16.py
@@ -1,11 +1,23 @@
-llista=[] #llista buida
-valor=int(input("Insereix un valor: ")) #insereixo el primer valor
-maxim=0
+# Lista para almacenar los números de la serie
+llista = []
+ 
+# Entrada de números hasta que el usuario ingrese -1
+valor = int(input("Dame una serie de números acabados en -1: "))
+ 
 while not valor == -1:
-    llista.append(valor) #sumo el valor a la llista
-    valor=int(input("Insereix un valor: ")) #vaig afegint valors
-    print(llista) #print per comprovar si els valors s'afegeixen a la llista
-    if valor > maxim:
-        maxim=valor
-        print(maxim)
-#for maxim in range
+    llista.append(valor)
+    valor = int(input("Dame una serie de números acabados en -1: "))
+ 
+# Encontramos el número más grande en la lista
+num_max = max(llista)
+ 
+# Encontramos los divisores del número más grande      
+div = []
+ 
+for num in llista:
+    if num_max % num == 0:
+        div.append(num)
+ 
+# Mostrar el resultado
+print(f"El número más grande es: {num_max}")
+print(f"Los divisores que se encuentran en la serie son: {', '.join(map(str, div))}")
